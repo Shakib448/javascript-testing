ad9070caaf84068dfbb727bf2a6020380fbb4f6a
Shakib448 <shakib448@gmail.com>
2021-02-07 10:59:41 +0600

    testing

--- __test__/util.test.js
+++ __test__/util.test.js
@@ -1,1 +1,6 @@
 const { generateText } = require("../util");
+
+test("SHould output name and age", () => {
+  const text = generateText("Max", 28);
+  expect(generateText("shakib", 23)).toBe(text);
+});
